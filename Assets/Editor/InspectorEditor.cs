737f243711bb2dd925c6e97a4506e44968d7d281
SMahdiFaghih <smahdifaghih2001@gmail.com>
2020-10-03 13:32:02 +0330

    added Targets

---
+++ Assets/Editor/InspectorEditor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Components)), CanEditMultipleObjects]
+public class InspectorEditor : Editor
+{
+    public SerializedProperty
+        targetComponent,
+        audioClips,
+        images,
+        text;
+    void OnEnable()
+    {
+        targetComponent = serializedObject.FindProperty("Target");
+        audioClips = serializedObject.FindProperty("AudioClips");
+        images = serializedObject.FindProperty("Images");
+        text = serializedObject.FindProperty("Text");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(targetComponent);
+
+        Localize.TargetComponent target = (Localize.TargetComponent)targetComponent.enumValueIndex;
+        switch (target)
+        {
+            case Localize.TargetComponent.AudioSource:
+                EditorGUILayout.PropertyField(audioClips, new GUIContent("AudioClips"));
+                break;
+            case Localize.TargetComponent.Image:
+                EditorGUILayout.PropertyField(images, new GUIContent("Images"));
+                break;
+            case Localize.TargetComponent.Text:
+                EditorGUILayout.PropertyField(text, new GUIContent("Text"));
+                break;
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+}
--- Assets/Scripts/Localize.cs
+++ Assets/Scripts/Localize.cs
@@ -1,19 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
 using RTLTMPro;
 
-[RequireComponent(typeof(RTLTextMeshPro))]
-public class LocalizeText : MonoBehaviour
+[System.Serializable]
+public struct Components
 {
+    public AudioClip[] AudioClips;
+    public Image[] Images;
     public LocalizedString localizedString;
+}
+
+public class Localize : MonoBehaviour
+{
+    public TargetComponent Target;
+    public Components Components;
 
     private RTLTextMeshPro RTLText;
 
+    public enum TargetComponent
+    {
+        AudioSource,
+        Image,
+        Text
+    }
+
     void Start()
     {
-        RTLText = GetComponent<RTLTextMeshPro>();
-        RTLText.text = localizedString.value;
+       
+        //RTLText = GetComponent<RTLTextMeshPro>();
+        //RTLText.text = localizedString.value;
         /*else
         {
             PersianText persianText = GetComponent<PersianText>();
