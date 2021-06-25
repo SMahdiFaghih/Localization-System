fa7efcf534332df4fead83769176cd2c93a773c4
SMahdiFaghih <smahdifaghih2001@gmail.com>
2021-06-25 22:26:22 +0430

    added sample scene and tutorial for SetKey function

---
+++ Assets/Scripts/Sample/TextLocalizationSample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextLocalizationSample : MonoBehaviour
+{
+    public Localize SetKeyTutorial_localize;
+    [Space]
+    public Localize SetKeyTutorialNewLine_localize;
+    [Space]
+    public Localize SetKeyAndReplaceStringsTutorial_localize;
+    public string[] ReplaceStrings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetKeyTutorial_localize.SetKey("sample_set_key_tutorial");
+        SetKeyTutorialNewLine_localize.SetKey("sample_set_key_new_line_tutorial");
+        SetKeyAndReplaceStringsTutorial_localize.SetKey("sample_set_key_replace_strings_tutorial", ReplaceStrings[0], ReplaceStrings[1]);
+    }
+
+}
